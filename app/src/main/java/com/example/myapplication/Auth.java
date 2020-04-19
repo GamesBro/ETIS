16ff47c4ab58a14a812640dde32c438b0edf578c
d0pel <we3lby@gmail.com>
2020-04-19 05:08:24 +0500

    Status bar design

--- app/src/main/java/com/example/myapplication/Auth.java
+++ app/src/main/java/com/example/myapplication/Auth.java
@@ -10,6 +10,7 @@ import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.EditText;
 
 import java.io.IOException;
@@ -21,6 +22,7 @@ public class Auth extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_auth);
+
     }
 
     @SuppressLint("StaticFieldLeak")
